af258e7357969dc311cc276879a0dd076026350b
Liushaozhe_Bulo <bulo1025@users.noreply.github.com>
2019-06-11 18:51:37 +0800

    codes

---
+++ Lesson2_Improving Deep Neural Networks/assignment3/tensorboard.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+y_hat = tf.constant(36, name='y_hat')            # Define y_hat constant. Set to 36.
+y = tf.constant(39, name='y')                    # Define y. Set to 39
+
+loss = tf.Variable((y - y_hat)**2, name='loss')  # Create a variable for the loss
+
+init = tf.global_variables_initializer()         # When init is run later (session.run(init)),
+                                                 # the loss variable will be initialized and ready to be computed
+with tf.Session() as session:                    # Create a session and print the output
+    writer = tf.summary.FileWriter("logs/", session.graph)
+    session.run(init)                            # Initializes the variables
+    print(session.run(loss))                     # Prints the loss
+
+tf.placeholder()
